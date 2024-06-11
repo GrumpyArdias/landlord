0f7ced1d66fc159f074c7dffd1ac734fbed5c82a
GrumpyArdias <this.mario@outlook.com>
2024-06-11 12:47:05 +0200

    inicial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+// Define your routes and middleware here
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
